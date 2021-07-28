cc7c1f4c82afe5c315bdb9b5b8c0832b95d24ea0
fionna <fionnatong@gmail.com>
2021-07-28 15:49:03 +0800

    Initial Commit

---
+++ FoodWaste/FoodWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodWasteApp.swift
+//  FoodWaste
+//
+//  Created by fionna on 28/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
