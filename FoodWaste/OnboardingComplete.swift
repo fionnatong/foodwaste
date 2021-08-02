410f8ee6a684a12ae0337fa40580c73f60d5eb85
Yihui <yihuihuizz@gmail.com>
2021-08-02 14:35:17 +0800

    [YH] Edit styling for onboarding success page

--- FoodWaste/OnboardingComplete.swift
+++ FoodWaste/OnboardingComplete.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct OnboardingComplete: View {
     var body: some View {
-        VStack{
-            Text("Awesome!")
-                .font(CustomFont.headerOne)
-            Text("Your food donations have been launched into the Marketplace")
-            Image("success")
-            Text("Check your Donation Inventory to see the status of your food donations.")
-            Text("We will notify you when an organisation has decided to adopt your food.")
+        
+        ZStack(alignment: .top){
+            Color("secondary").edgesIgnoringSafeArea(.all)
+            Spacer().padding(.top, 24)
+            VStack{
+                VStack(spacing: 8) {
+                    Text("Awesome!")
+                        .font(CustomFont.headerOne)
+                    Text("Your food donations have been launched into the Marketplace.")
+                        .font(CustomFont.bodyRegular)
+                        .multilineTextAlignment(.center)
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+                .padding(.horizontal, 24)
+               
+                Image("success")
+                
+                VStack{
+                    Text("Check your Donation Inventory to see the status of your food donations.")
+                        .font(CustomFont.bodyRegular)
+                        .multilineTextAlignment(.center)
+                    Text("We will notify you when an organisation has decided to adopt your food.")
+                        .font(CustomFont.bodyRegular)
+                        .multilineTextAlignment(.center)
+                }
+                .padding(.horizontal, 24)
+            }
+            .padding(.top, 24)
         }
     }
 }
