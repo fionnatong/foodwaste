e11b364d646b3dfa8914ce674d1ce870550a9fc4
Leong Wei Jian <weijian19391@gmail.com>
2021-08-03 08:50:52 +0800

    [weijian] update checkbox

--- FoodWaste/Components/Checkbox.swift
+++ FoodWaste/Components/Checkbox.swift
@@ -15,25 +15,23 @@ struct Checkbox: View {
                     .padding(.init(top: 18, leading: 29, bottom: 18, trailing: 18))
                 Text(title)
                     .font(CustomFont.bodyRegular)
-                    .foregroundColor(Color("text"))
                 Spacer()
             }
         })
         .overlay(
             RoundedRectangle(cornerRadius: 16)
                 .stroke(Color("primary"), lineWidth: isChecked ? 3 : 0)
+                
         )
+        .foregroundColor(Color("text"))
         .background(Color.white .cornerRadius(16) .shadow(color: Color("shadow"), radius: 12, x: 0, y: 4) )
-        .padding(.bottom, 28)
-        
-        
     }
 }
 
 struct Checkbox_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            Checkbox(title: "Monday", isChecked: false)
+            Checkbox(title: "Monday", isChecked: false).padding(.bottom, 20)
             Checkbox(title: "Tuesday", isChecked: true)
         }
     }
